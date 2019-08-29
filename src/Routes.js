48895a5d3dfc764da8e785848003218346cafe33
SeanKim <sean.kim84@gmail.com>
2019-08-29 10:57:05 +0900

    For fixing

---
+++ src/Routes.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Signup from './user/Signup';
+import Signin from './user/Signin';
+import Home  from './core/Home';
+import PrivateRoute from './auth/PrivateRoute';
+import AdminRoute from './auth/AdminRoute';
+import DashBoard from './user/UserDashboard';
+import AdminDashBoard from './user/AdminDashboard';
+import AddCategory from './admin/AddCategory';
+import AddProduct from './admin/AddProduct';
+import Shop from './core/Shop';
+import Product from './core/Product';
+import Cart from './core/Cart';
+
+const Routes = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path='/signin' exact component={Signin} />
+                <Route path='/signup' exact component={Signup} />
+                <Route path='/' exact component={Home} />
+                <Route path='/shop' exact component={Shop} />
+                <Route path='/product/:productId' exact component={Product} />
+                <Route path='/cart' exact component={Cart} />
+                <PrivateRoute path="/user/dashboard" exact component={DashBoard} />
+                <AdminRoute path="/admin/dashboard" exact component={AdminDashBoard} />
+                <AdminRoute path="/create/category" exact component={AddCategory} />
+                <AdminRoute path="/create/product" exact component={AddProduct} />
+            </Switch>
+        </BrowserRouter>
+    );
+};
+
+export default Routes;
+
---
+++ src/core/Checkout.js
@@ -0,0 +1,137 @@
+import React, { useState, useEffect } from "react";
+import {
+    getProducts,
+    getBraintreeClientToken,
+    processPayment
+} from "./apiCore";
+import Card from "./Card";
+import { isAuthenticated } from "../auth";
+import { Link } from "react-router-dom";
+import "braintree-web";
+import DropIn from "braintree-web-drop-in-react";
+
+const Checkout = ({ products }) => {
+    const [data, setData] = useState({
+        success: false,
+        clientToken: null,
+        error: "",
+        instance: {},
+        address: ""
+    });
+
+    const userId = isAuthenticated() && isAuthenticated().user._id;
+    const token = isAuthenticated() && isAuthenticated().token;
+
+    const getToken = (userId, token) => {
+        getBraintreeClientToken(userId, token).then(data => {
+            if (data.error) {
+                setData({ ...data, error: data.error });
+            } else {
+                setData({ clientToken: data.clientToken });
+            }
+        });
+    };
+
+    useEffect(() => {
+        getToken(userId, token);
+    }, []);
+
+    const getTotal = () => {
+        return products.reduce((currentValue, nextValue) => {
+            return currentValue + nextValue.count * nextValue.price;
+        }, 0);
+    };
+
+    const showCheckout = () => {
+        return isAuthenticated() ? (
+            <div>{showDropIn()}</div>
+        ) : (
+            <Link to="/signin">
+                <button className="btn btn-primary">Sign in to checkout</button>
+            </Link>
+        );
+    };
+
+    const buy = () => {
+        // send the nonce to your server
+        // nonce = data.instance.requestPaymentMethod()
+        let nonce;
+        let getNonce = data.instance
+            .requestPaymentMethod()
+            .then(data => {
+                // console.log(data);
+                nonce = data.nonce;
+                // once you have nonce (card type, card number) send nonce as 'paymentMethodNonce'
+                // and also total to be charged
+                // console.log(
+                //     "send nonce and total to process: ",
+                //     nonce,
+                //     getTotal(products)
+                // );
+                const paymentData = {
+                    paymentMethodNonce: nonce,
+                    amount: getTotal(products)
+                };
+
+                processPayment(userId, token, paymentData)
+                    .then(response => {
+                        // console.log(response
+                        setData({ ...data, success: response.success });
+                        // empty cart
+                        // create order
+                    })
+                    .catch(error => console.log(error));
+            })
+            .catch(error => {
+                // console.log("dropin error: ", error);
+                setData({ ...data, error: error.message });
+            });
+    };
+
+    const showDropIn = () => (
+        <div onBlur={() => setData({ ...data, error: "" })}>
+            {data.clientToken !== null && products.length > 0 ? (
+                <div>
+                    <DropIn
+                        options={{
+                            authorization: data.clientToken
+                        }}
+                        onInstance={instance => (data.instance = instance)}
+                    />
+                    <button onClick={buy} className="btn btn-success btn-block">
+                        Pay
+                    </button>
+                </div>
+            ) : null}
+        </div>
+    );
+
+    const showError = error => (
+        <div
+            className="alert alert-danger"
+            style={{ display: error ? "" : "none" }}
+        >
+            {error}
+        </div>
+    );
+
+    const showSuccess = success => (
+        <div
+            className="alert alert-info"
+            style={{ display: success ? "" : "none" }}
+        >
+            Thanks! Your payment was successful!
+        </div>
+    );
+
+    return (
+        <div>
+            <h2>Total: ${getTotal()}</h2>
+            {showSuccess(data.success)}
+            {showError(data.error)}
+            {showCheckout()}
+        </div>
+    );
+};
+
+export default Checkout;
